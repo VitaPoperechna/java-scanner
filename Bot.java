56ba78da789cb8d9058283ba7963f6a438329b1a
VitaPoperechna <vitapoperechna@gmal.com>
2025-05-06 16:06:14 +0300

    fix nextLine trap

--- Bot.java
+++ Bot.java
@@ -1,33 +1,36 @@
+import java.util.Scanner;
+
 public class Bot {
     public static void main(String[] args) {
         //Crea el scanner
-
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hola, ¿Cómo te llamas?");
-
+        String name = scanner.nextLine();
         //Recoge el dato
         //Añade una nueva línea antes de mostrar la siguiente pregunta.
 
-        System.out.println("Hola <name>! Soy FemBot. ¿De qué ciudad eres?");
-
+        System.out.println("Hola " + name + "! Soy FemBot. ¿De qué ciudad eres?");
+        String location = scanner.nextLine();
         //Recoge el dato
         //Añade una nueva línea antes de mostrar la siguiente pregunta.
 
-        System.out.println("He escuchado que <location> es un encanto. Yo nací en Oracle city.");
+        System.out.println("He escuchado que " + location + " es un encanto. Yo nací en Oracle city.");
         System.out.println("\n¿Cuántos años tienes?");
-
+        int age = scanner.nextInt();
+        scanner.nextLine();
         //Recoge el dato
         //Añade una nueva línea antes de mostrar la siguiente pregunta.
 
-        System.out.println("Entonces me dices que tienes <age> años. Yo tengo 400 años.");
-        System.out.println("Eso quiere decir que soy <divide 400 por tu edad> veces más vieja que tu.");
+        System.out.println("Entonces me dices que tienes " + age + " años. Yo tengo 400 años.");
+        int divide = 400 / age;
+        System.out.println("Eso quiere decir que soy " + divide + " veces más vieja que tu.");
         System.out.println("Basta de hablar de mi misma. ¿Cuál es tu lenguaje de programación favorito? (Por favor no digas Python)");
-
+        String language = scanner.nextLine();
         //Recoge el dato
         //Añade una nueva línea.
-        System.out.println("<language>, ¡Eso es excelente! Mucho gusto haber hablado contigo <name>, ¡Hablamos luego!");
-
+        System.out.println(language + ", ¡Eso es excelente! Mucho gusto haber hablado contigo " + name + ", ¡Hablamos luego!");
         //¿Te acordaste del nextLine trap?
         //Recuerda cerrar el scanner
-
+        scanner.close();
     }
 }
